bac83ffefebab7123b7fd8d45efca3935c9f47d7
michaelvianas <vmichael116@gmail.com>
2023-06-11 17:31:05 -0300

    Carregamento de arquivos

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
+import fs from "fs";
 import chalk from "chalk";
+import { error } from "console";
 
-console.log(chalk.blue('olá mundo'));
+function tratarErro(erro){
+  console.log(erro);
+  throw new Error(chalk.bgRedBright(erro.code, 'não há arquivo no diretório'));
+}
 
-console.log(chalk.red('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/'));
-console.log(chalk.green('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/'));
+async function pegarArquivo(caminhoDoArquivo){
+  try{
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(chalk.yellow(texto));
+  }catch(erro){
+    tratarErro(erro)
+  }finally{
+    console.log(chalk.green(`Operação concluída!`));
+  }
+}
+
+// function pegarArquivo(caminhoDoArquivo){
+//   const encoding = 'utf-8';
+
+//   fs.promises.readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch(tratarErro)
+// }
+
+// function pegarArquivo(caminhoDoArquivo){
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//     if(erro){
+//       tratarErro(erro);
+//     }
+//     console.log(chalk.green(texto))
+//   })
+// }
+
+pegarArquivo('./arquivos/texto.md');
